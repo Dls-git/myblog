b2c762a78c2d5e639d7054971be60960997c75f6
Dls-git <287996978@qq.com>
2025-12-20 14:49:02 +0800

    fix sth

--- src/posts/dataJs/friendList.js
+++ src/posts/dataJs/friendList.js
@@ -3,19 +3,19 @@ export const friendsList = [
         name: 'GSSD_NrxRobot404',
         desc: '茉莉花茶我还喜欢',
         avatar: new URL('../../assets/friendsAvatar/GSSD_NrxRobot404.jpg', import.meta.url).href,
-        link: '#'
+        link: "/null"
     },
     {
         name: "D",
         desc: "群起而攻之",
         avatar:  new URL('../../assets/friendsAvatar/D.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: 'Lemon &X.',
         desc: '自由的鸟哪怕身处牢笼也闪烁着自由的光芒',
         avatar: new URL('../../assets/friendsAvatar/Lemon&X.jpg', import.meta.url).href,
-        link: '#'
+        link: "/null"
     },
     {
         "name": "Sejolyn",
@@ -24,75 +24,81 @@ export const friendsList = [
         "avatar": "https://sejolyn.fyi/avatar.jpg"
     },
     {
+        "name": "Echo昭",
+        "desc": "铮铮，昂扬",
+        "link": "/null",
+        "avatar": new URL('../../assets/friendsAvatar/echo.jpg', import.meta.url).href
+    },
+    {
         name: 'hsns128',
         desc: '或选择你的个性签名',
         avatar: new URL('../../assets/friendsAvatar/hsns128.jpg', import.meta.url).href,
-        link: '#'
+        link: "/null"
     },
     {
         name: "FLCL",
         desc: "200%",
         avatar: new URL('../../assets/friendsAvatar/FLCL.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "翁呀跋",
         desc: "DBAA",
         avatar: new URL('../../assets/friendsAvatar/wyb.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "Tulmo",
         desc: "sui!",
         avatar: new URL('../../assets/friendsAvatar/Tulmo.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "Eternal海川",
         desc: "null",
         avatar: new URL('../../assets/friendsAvatar/Eternal.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "Galaxy",
         desc: "null",
         avatar:new URL('../../assets/friendsAvatar/Galaxy.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "断东河",
         desc: "不够勇敢",
         avatar: new URL('../../assets/friendsAvatar/yqj.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "Oxygen",
         desc: "Oxygen need Anoxia",
         avatar:  new URL('../../assets/friendsAvatar/wym.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "找自己",
         desc: "只今只道只今句，梅子熟时栀子香",
         avatar:  new URL('../../assets/friendsAvatar/zzj.jpg', import.meta.url).href,
-        link: "#"
+        link: "/null"
     },
     {
         name: "Friend 10",
         desc: "A cool friend into fitness.",
         avatar: "",
-        link: "#"
+        link: "/null"
     },
     {
         name: "Friend 11",
         desc: "A cool friend who enjoys writing.",
         avatar: "",
-        link: "#"
+        link: "/null"
     },
     {
         name: "Friend 12",
         desc: "A cool friend passionate about movies.",
         avatar: "",
-        link: "#"
+        link: "/null"
     }
 ]
